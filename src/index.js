8367701d7324274aea08627dd1e7fd14b7fd8047
Rita Bradley <contact@ritabradley.com>
2019-02-23 08:55:55 -0800

    create component to list cards

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { cats } from './cats';
 
-ReactDOM.render(
-  <div>
-    <Card id={cats[0].id} name={cats[0].name} email={cats[0].email} />
-    <Card id={cats[1].id} name={cats[1].name} email={cats[1].email} />
-    <Card id={cats[2].id} name={cats[2].name} email={cats[2].email} />
-  </div>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<CardList cats={cats} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
