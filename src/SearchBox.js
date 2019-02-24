e4e2c06d6824e2d6942da06d40a0794c565b6d07
Rita Bradley <contact@ritabradley.com>
2019-02-23 19:06:13 -0800

    set state to make cats searchable

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchField, searchChange }) => {
   return (
     <div className='pa2'>
       <input
+        onChange={searchChange}
         className='pa3 ba b--purple bg-light-pink input-reset'
         type='search'
-        placeholder='search friends'
+        placeholder='search cats'
       />
     </div>
   );
