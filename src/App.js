03897ef679cf1b6f33dd315bfcb7d27ace12b5fe
Rita Bradley <contact@ritabradley.com>
2019-02-23 17:24:23 -0800

    create searchbox component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { cats } from './cats';
 
 const App = () => {
   return (
-    <>
+    <div className='tc'>
       <h1>CuteCatFriends</h1>
+      <SearchBox />
       <CardList cats={cats} />
-    </>
+    </div>
   );
 };
 
---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div className='pa2'>
+      <input
+        className='pa3 ba b--purple bg-light-pink input-reset'
+        type='search'
+        placeholder='search friends'
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
