8552ff3b85facd2cf5b9ba0ca25ca4fe6e79ba43
Rita Bradley <contact@ritabradley.com>
2020-01-20 23:36:54 -0800

    Configure tailwind

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+
+class App extends Component {
+    constructor(params) {
+        super();
+
+        this.state = {
+            string: 'Rita Bradley'
+        };
+    }
+
+    render() {
+        return <div className='App container font-sans'>Hello! My name is {this.state.string}</div>;
+    }
+}
+
+export default App;
