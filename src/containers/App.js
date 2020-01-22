9dff9634b20d96c1648bc26e1000902b872d75e0
Rita Bradley <contact@ritabradley.com>
2020-01-21 20:55:39 -0800

    connect API; added CardList and Card files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
+const URL = 'https://jsonplaceholder.typicode.com/users';
+
 class App extends Component {
     constructor(params) {
         super();
 
         this.state = {
-            string: 'Rita Bradley'
+            monsters: []
         };
     }
 
+    componentDidMount() {
+        fetch(URL)
+            .then(res => res.json())
+            .then(users => this.setState({ monsters: users }))
+            .catch(err => console.log(`Something went wrong: ${err}`));
+    }
+
     render() {
-        return <div className='App container font-sans'>Hello! My name is {this.state.string}</div>;
+        return (
+            <div className='App container mx-auto font-sans'>
+                <ul className='text-center font-bold text-lg text-gray-700 list-none'>
+                    {this.state.monsters.map((monster, i) =>
+                        i % 2 === 0 ? (
+                            <li key={monster.name} className='border border-gray-300'>
+                                {monster.name}
+                            </li>
+                        ) : (
+                            <li key={monster.name} className='border border-gray-300 bg-teal-100'>
+                                {monster.name}
+                            </li>
+                        )
+                    )}
+                </ul>
+            </div>
+        );
     }
 }
 
